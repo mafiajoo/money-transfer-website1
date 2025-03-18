380b0ea8baf3e4768723cc6f3310d8dcf639aeea
Mafiajoo <mafiajoo100@gmail.com>
2025-03-18 20:21:25 +0100

    Rebuild Netlify setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
-
+export default {
+    srcDir: 'src', // Ensures Astro knows where your files are
+    output: 'server',
+    adapter: '@astro/netlify',
+  };
+  
