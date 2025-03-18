0be741ec8f984f1a4e6526fc856a3d2e2ec3bc93
Mafiajoo <mafiajoo100@gmail.com>
2025-03-18 20:44:47 +0100

    Fix Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify"; // Import the Netlify adapter
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: "server",
-  adapter: netlify(), // Use the adapter correctly
+  adapter: netlify(),
 });
