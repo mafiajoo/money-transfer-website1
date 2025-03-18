d536cf55d0b458a839278c60c018ce91ad3a9c55
Mafiajoo <mafiajoo100@gmail.com>
2025-03-18 20:40:29 +0100

    Fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-export default {
-    srcDir: 'src', // Ensures Astro knows where your files are
-    output: 'server',
-    adapter: netlify()
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify"; // Import the Netlify adapter
+
+export default defineConfig({
+  output: "server",
+  adapter: netlify(), // Use the adapter correctly
+});
